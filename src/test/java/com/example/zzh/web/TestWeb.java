bcebd1b7d458c2e3f7b30ef6b3ef985990348104
zhaozhihong <zhihong.zhao@melot.cn>
2020-06-09 19:00:57 +0800

    修改

--- src/test/java/com/example/zzh/web/TestWeb.java
+++ src/test/java/com/example/zzh/web/TestWeb.java
@@ -3,7 +3,6 @@ package com.example.zzh.web;
 import com.example.zzh.test.optimized.D;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 import java.io.File;
@@ -18,7 +17,7 @@ public class TestWeb extends D {
     public void test(){
         File file = new File("driver/chromedriver");
         String absolutePath = file.getAbsolutePath();
-        System.setProperty("webdriver.chrome.driver","driver/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.get("http://www.baidu.com");
     }
